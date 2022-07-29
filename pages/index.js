eba5ad66a001ede5babe1c084342883d8ea6f942
PIEWSIRI <piew.siripon@gmail.com>
2022-07-29 17:02:12 +0700

    slick

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,29 @@ import Layout from '../components/Layout';
 import ProductItem from '../components/ProductItem';
 import Product from '../models/Product';
 import db from '../utils/db';
-
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 export default function Home({ products }) {
+  const settings = {
+    className: 'center',
+    centerMode: true,
+    infinite: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    speed: 500,
+  };
   return (
     <Layout title="Home Page">
       <h1 className="text-xl mt-10 px-4">ประกาศ</h1>
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4 mt-5">
-        {products.map((product) => (
-          <ProductItem product={product} key={product.slug}></ProductItem>
-        ))}
-      </div>
+      <img src="../../images/yellowtag1.png" width="300" height="300" />
+      <Slider {...settings}>
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4 mt-5">
+          {products.map((product) => (
+            <ProductItem product={product} key={product.slug}></ProductItem>
+          ))}
+        </div>
+      </Slider>
     </Layout>
   );
 }
