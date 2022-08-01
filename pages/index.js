dd268b7190389dea5513c690030fca062b76c6e2
PIEWSIRI <piew.siripon@gmail.com>
2022-08-01 14:57:17 +0700

    Layout

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import db from '../utils/db';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+
 export default function Home({ products }) {
   const settings = {
     dots: true,
