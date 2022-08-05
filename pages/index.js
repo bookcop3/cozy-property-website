3f0b4743b65dae449bcdd456ea71db6e540ca1c5
bookcop3 <108032328+bookcop3@users.noreply.github.com>
2022-08-06 00:40:24 +0700

    uploard

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,13 @@
 import Layout from '../components/Layout';
-import ProductItem from '../components/ProductItem';
 import Product from '../models/Product';
 import db from '../utils/db';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
 
-export default function Home({ products }) {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-  };
+export default function Home() {
   return (
     <Layout title="Home Page">
       <div className="premium">
         <h1 className="text-xl mt-10 px-4">ประกาศ</h1>
         <img src="../../images/yellowtag1.png" width="300" height="300" />
-      </div>
-      <div>
-        <Slider {...settings}>
-          {products.map((product) => (
-            <ProductItem
-              product={product}
-              key={product.product_type === 'บ้าน'}
-            ></ProductItem>
-          ))}
-        </Slider>
       </div>
     </Layout>
   );
