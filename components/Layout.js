bb0769f79baba443bac308c3393d667af681d4a6
homsrc <109324151+homsrc@users.noreply.github.com>
2022-08-15 13:40:09 +0700

    .

--- components/Layout.js
+++ components/Layout.js
@@ -194,30 +194,6 @@ export default function Layout({ title, children }) {
         </div>
         <main className="container m-auto mt-20 px-4">{children}</main>
       </div>
-      <footer className="flex h-5 shadow-md">
-        <p className="p-10">
-          ติดต่อเรา
-          <p>cozyproperty.th@gmail.com</p>
-          <p>+66</p>
-          <img
-            src="../../images/contactIcon.png"
-            width="170"
-            height="170"
-            alt="contact"
-          />
-        </p>
-        <p className="p-10">
-          เกี่ยวกับเรา
-          <p>ลงโฆษณา Banner</p>
-          <p>คูมือการใช้งาน</p>
-        </p>
-        <p className="p-10">
-          บทความ/เนื้อหา
-          <p>เนื้อหา</p>
-          <p>วิธีการลงประกาศ</p>
-          <p>About Asset</p>
-        </p>
-      </footer>
     </>
   );
 }
